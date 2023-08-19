da32ce1b3922ca173b0f271715c99353318115a6
Juliano Américo <juliano.americo.dev@gmail.com>
2023-08-18 23:43:58 -0300

    feat: configure json property for response feign

--- src/main/java/com/cleanarch/example/gateway/feign/client/model/AddressFeignResponse.java
+++ src/main/java/com/cleanarch/example/gateway/feign/client/model/AddressFeignResponse.java
@@ -1,6 +1,9 @@
 package com.cleanarch.example.gateway.feign.client.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record AddressFeignResponse(
+        @JsonProperty("localidade")
         String city
 ) {
 }
